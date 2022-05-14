480da8cfd270dd9f9938a7015a28c3f254a10338
TheyKay <murraykyleb@gmail.com>
2022-05-14 14:53:03 -0600

    account stylings, deleted account.js

--- scripts/account.js
+++
@@ -1,7 +0,0 @@
-const gears = document.getElementsByClassName('fa-gear');
-
-Array.from(gears).forEach(gear => {
-  gear.addEventListener('click', e => {
-    console.log(e.target.previousSibling)
-  })
-});
